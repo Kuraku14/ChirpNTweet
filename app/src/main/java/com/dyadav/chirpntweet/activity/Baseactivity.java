82dd44e59e8c9761abee93a0afece0c0fdf45440
Divya Yadav <divyayadav1606@gmail.com>
2017-03-30 00:27:17 -0700

    activity rename

--- app/src/main/java/com/dyadav/chirpntweet/activity/Baseactivity.java
+++ app/src/main/java/com/dyadav/chirpntweet/activity/Baseactivity.java
@@ -2,7 +2,7 @@ package com.dyadav.chirpntweet.activity;
 
 import android.support.v7.app.AppCompatActivity;
 
-public abstract class  Baseactivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
 //Get User info for Top icon and set an Intent to launch Profile Activity
 }
